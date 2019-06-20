fd1e50bc22c5c73542779a5862e0b7a4a44e625b
Daniel Coady <daniel.coady@outlook.com.au>
2019-06-20 16:02:25 +1000

    initial commit

---
+++ main.js
@@ -0,0 +1,57 @@
+const Discord = require('discord.js');
+const settings = require('./settings.json');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.isMentioned(client.user)) {
+    // find the verified role for the server
+    var verifiedRole = null;
+    for (const role of msg.guild.roles) {
+      if (role[1].name == 'Verified') {
+        verifiedRole = role[0];
+        break;
+      }
+    }
+
+    if (verifiedRole == null) {
+      msg.channel.send("**ERROR:** Verified role not found");
+    } else {
+      // check if the user already has the verified role
+      var isVerified = false;
+      for (const role of msg.member.roles) {
+        if (role[1].name == 'Verified') {
+          isVerified = true;
+          break;
+        }
+      }
+
+      if (isVerified) { // user already has role
+        msg.channel.send(
+          '**VERIFICATION REQUEST RECIEVED**' +
+          '\n**User:** `' + msg.author.tag + '`' +
+          '\n**Verified**: `TRUE`\n' +
+          'No further action required'
+        );
+      } else { // user does not already have role
+        msg.member.addRole(verifiedRole);
+
+        msg.channel.send(
+          '**VERIFICATION REQUEST RECIEVED**' +
+          '\n**User:** `' + msg.author.tag + '`' +
+          '\n**Verified**: `FALSE`' +
+          '\nVerifying...\n' + 
+          'Done\n' +
+          'Welcome `' + msg.author.username + '`'
+        );
+      }
+    }
+
+    msg.delete();
+  }
+});
+
+client.login(settings.token);
