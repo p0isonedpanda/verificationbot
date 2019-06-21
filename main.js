510850590dcd5147fa67972b913988e4b83000d8
Daniel Coady <daniel.coady@outlook.com.au>
2019-06-21 17:14:06 +1000

    refactored code to be easier to read

--- main.js
+++ main.js
@@ -7,53 +7,59 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+  if (msg.author == client.user) return;
+
   if (msg.isMentioned(client.user)) {
-    // find the verified role for the server
-    var verifiedRole = null;
-    for (const role of msg.guild.roles) {
+    VerifyUser(msg);
+  }
+});
+
+function VerifyUser(msg)
+{
+  // find the verified role for the server
+  var verifiedRole = null;
+  for (const role of msg.guild.roles) {
+    if (role[1].name == 'Verified') {
+      verifiedRole = role[0];
+      break;
+    }
+  }
+
+  if (verifiedRole == null) {
+    msg.channel.send("**ERROR:** Verified role not found");
+  } else {
+    // check if the user already has the verified role
+    var isVerified = false;
+
+    for (const role of msg.member.roles) {
       if (role[1].name == 'Verified') {
-        verifiedRole = role[0];
+        isVerified = true;
         break;
       }
     }
 
-    if (verifiedRole == null) {
-      msg.channel.send("**ERROR:** Verified role not found");
-    } else {
-      // check if the user already has the verified role
-      var isVerified = false;
-      try {
-        for (const role of msg.member.roles) {
-          if (role[1].name == 'Verified') {
-            isVerified = true;
-            break;
-          }
-        }
-      } catch { } // hacky af but i need this working now
+    if (isVerified) { // user already has role
+      msg.channel.send(
+        '**VERIFICATION REQUEST RECIEVED**' +
+        '\n**User:** `' + msg.author.tag + '`' +
+        '\n**Verified**: `TRUE`\n' +
+        'No further action required'
+      );
+    } else { // user does not already have role
+      msg.member.addRole(verifiedRole);
 
-      if (isVerified) { // user already has role
-        msg.channel.send(
-          '**VERIFICATION REQUEST RECIEVED**' +
-          '\n**User:** `' + msg.author.tag + '`' +
-          '\n**Verified**: `TRUE`\n' +
-          'No further action required'
-        );
-      } else { // user does not already have role
-        msg.member.addRole(verifiedRole);
-
-        msg.channel.send(
-          '**VERIFICATION REQUEST RECIEVED**' +
-          '\n**User:** `' + msg.author.tag + '`' +
-          '\n**Verified**: `FALSE`' +
-          '\nVerifying...\n' + 
-          'Done\n' +
-          'Welcome `' + msg.author.username + '`'
-        );
-      }
+      msg.channel.send(
+        '**VERIFICATION REQUEST RECIEVED**' +
+        '\n**User:** `' + msg.author.tag + '`' +
+        '\n**Verified**: `FALSE`' +
+        '\nVerifying...\n' + 
+        'Done\n' +
+        'Welcome `' + msg.author.username + '`'
+      );
     }
-
-    msg.delete();
   }
-});
+
+  msg.delete();
+}
 
 client.login(settings.token);
